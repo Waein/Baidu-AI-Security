b637a994f37ee025ce7fa61e8a242361194db250
Haein <bwl@fuhengkeji.com>
2018-11-06 13:59:44 +0800

    change code for application

--- src/main/java/com/wjw/Application.java
+++ src/main/java/com/wjw/Application.java
@@ -1,14 +1,13 @@
 package com.wjw;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 //@EnableAutoConfiguration
-public class SecurityApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SecurityApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
--- src/test/java/com/wjw/security/ApplicationTests.java
+++ src/test/java/com/wjw/security/ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SecurityApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
